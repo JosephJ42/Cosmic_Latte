37b2eecbea3921bc3d38a3fcf5c144a5d3d6c31c
Joseph Jarvis <89016282+JosephJ42@users.noreply.github.com>
2022-04-08 09:56:53 +0100

    Manual file upload of iOS App

---
+++ Cosmic Latte/Cosmic Latte/Cosmic_LatteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cosmic_LatteApp.swift
+//  Cosmic Latte
+//
+//  Created by Joseph Jarvis on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Cosmic_LatteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
