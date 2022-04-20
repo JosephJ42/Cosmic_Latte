c6e1a2d4c227f7ec2c61bc549ca4fc31ff7782e5
Joseph Jarvis <89016282+JosephJ42@users.noreply.github.com>
2022-04-20 13:46:06 +0100

    initial implementation of model view class

---
+++ Cosmic Latte/Cosmic Latte/CosmicLatteViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CosmicLatteViewModel.swift
+//  Cosmic Latte
+//
+//  Created by Joseph Jarvis on 20/03/2022.
+//
+
+import Foundation
+
+private let defaultMoon = ""
+private let moonImage = ["",""]
+
+
